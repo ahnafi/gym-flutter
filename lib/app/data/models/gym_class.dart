eaa4c05c3f2ce8d5d65d231009bd017758fbeba4
athallahdx <athallahgamer4@gmail.com>
2025-12-14 23:15:59 +0700

    Refactor GymClass: update fromJson method to handle images as either String or List

--- lib/app/data/models/gym_class.dart
+++ lib/app/data/models/gym_class.dart
@@ -24,6 +24,15 @@ class GymClass {
   });
 
   factory GymClass.fromJson(Map<String, dynamic> json) {
+    // Handle images as either String or List
+    String imageString;
+    if (json['images'] is List) {
+      final imagesList = json['images'] as List;
+      imageString = imagesList.isNotEmpty ? imagesList[0] as String : '';
+    } else {
+      imageString = json['images'] as String;
+    }
+
     return GymClass(
       id: json['id'] as int,
       code: json['code'] as String,
@@ -31,7 +40,7 @@ class GymClass {
       slug: json['slug'] as String,
       description: json['description'] as String,
       price: (json['price'] as num).toInt(),
-      images: json['images'] as String,
+      images: imageString,
       status: json['status'] as String,
       createdAt: DateTime.parse(json['created_at'] as String),
       updatedAt: DateTime.parse(json['updated_at'] as String),
